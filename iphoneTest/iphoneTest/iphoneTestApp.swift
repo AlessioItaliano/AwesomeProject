49c5ab707cd4b53d5b3d845f7f8aa5a4e6e99edd
Vasyl Lepish <117004345+AlessioItaliano@users.noreply.github.com>
2024-01-21 15:34:28 +0200

    first hw

---
+++ iphoneTest/iphoneTest/iphoneTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphoneTestApp.swift
+//  iphoneTest
+//
+//  Created by Vasyl L on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct iphoneTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
